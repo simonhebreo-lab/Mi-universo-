42ea0a38c7fdf6a5c7bcbe20e4d1b91002e04b38
simonhebreo-lab <simonhebreo@gmail.com>
2026-09-22 07:20:12 -0500

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,8 +1,8 @@
-import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.169.0/build/three.module.js";
+import * as THREE from "three";
 
 import {
     OrbitControls
-} from "https://cdn.jsdelivr.net/npm/three@0.169.0/examples/jsm/controls/OrbitControls.js";
+} from "three/addons/controls/OrbitControls.js";
 
 // =====================================================
 // 💌 AQUÍ ESCRIBES TUS MENSAJES
